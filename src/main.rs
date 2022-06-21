0ad5285eee59543bdd08da09941d68192b920a78
Janmajaya Mall <janmajaya.choithram@gmail.com>
2022-06-21 19:20:23 +0530

    added ECC

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
+use std::marker::PhantomData;
+
 use halo2_proofs::{
-    circuit::Region,
+    circuit::{Region, AssignedCell, layouter, Layouter},
     poly::{Rotation }, arithmetic::{FieldExt, Field},
-    plonk::{Column, Advice, Selector, ConstraintSystem}
+    plonk::{Column, Advice, Selector, ConstraintSystem, Constraints}
         
 };
 
-pub struct Config {
+pub struct ECCPoint<F: FieldExt> {
+    x: AssignedCell<F, F>,
+    y: AssignedCell<F, F>
+}
+
+pub struct Config<F> {
     q_add: Selector,
     lambda: Column<Advice>,
     x_p: Column<Advice>,
@@ -16,11 +23,12 @@ pub struct Config {
     beta: Column<Advice>,
     gamma: Column<Advice>,
     delta: Column<Advice>,
+    _market: PhantomData<F>
 }
 
-impl Config {
+impl<F: FieldExt> Config<F> {
     fn configure(
-        meta: &mut ConstraintSystem<impl Field>,
+        meta: &mut ConstraintSystem<F>,
         lambda: Column<Advice>,
         x_p: Column<Advice>,
         y_p: Column<Advice>,
@@ -30,10 +38,40 @@ impl Config {
         beta: Column<Advice>,
         gamma: Column<Advice>,
         delta: Column<Advice>
-    ) {
+    ) -> Self {
         let q_add = meta.selector();
 
-        // meta.create_gate("add", ||)
+        meta.create_gate("add", |meta|{
+            let q_add = meta.query_selector(q_add);
+            let x_p = meta.query_advice(x_p, Rotation::cur());
+            let y_p = meta.query_advice(y_p, Rotation::cur());
+
+            /// TODO: finish the gate
+            [q_add * (x_p * y_p)]
+        });
+
+        Config {
+            q_add,
+            lambda,
+            x_p,
+            y_p,
+            x_qr,
+            y_qr,
+            alpha,
+            beta,
+            gamma,
+            delta,
+            _market: PhantomData,
+        }
+    }
+
+    fn assign(
+        layouter: &mut impl Layouter<F>,
+        p: ECCPoint<F>,
+        q: ECCPoint<F>
+    ) {
+        // assignt the region
+        // layouter.assign_region(name, assignment)
     }
 }
 
